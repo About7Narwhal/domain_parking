f7a4b833b35cafd7a0d071bc1cdc3bc02a5ca120
About7Narwhal <111166169+About7Narwhal@users.noreply.github.com>
2023-09-07 12:46:37 -0400

    Update ParkingNotice.js

--- src/components/ParkingNotice.js
+++ src/components/ParkingNotice.js
@@ -1,7 +1,7 @@
 import './ParkingNotice.css';
 
 const ParkingNotice = () => {
-  return <h1 className='parking_notice'>This domain is parked. Check back later. </h1>;
+  return <h1 className='parking_notice'>This domain is parked for future use.</h1>;
 };
 
 export default ParkingNotice;
